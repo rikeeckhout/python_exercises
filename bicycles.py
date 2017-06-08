d300f2cf25ecad99392eda15378b0fc19e0aacca
Rik Eeckhout <reeckhout@fednot.be>
2017-06-08 11:59:26 +0200

    initial commit

---
+++ bicycles.py
@@ -0,0 +1,13 @@
+bicycles=['trek','cannondale','redline','specialized']
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])
+print(bicycles[-2])
+print(bicycles[-3])
+print(bicycles)
+
+bicycles.reverse()
+print(bicycles)
+
